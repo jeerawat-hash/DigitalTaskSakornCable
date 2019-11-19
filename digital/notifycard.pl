f6fc1e86ee473a0192a774169aacb0d99d74376b
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-19 12:37:17 +0700

    add

--- digital/notifycard.pl
+++ digital/notifycard.pl
@@ -6,7 +6,7 @@
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/camessageedit.do';
 
-  my ($name) = @ARGV;
+  my ($card,$message,$date,$time) = @ARGV;
   #$name = '9980003200006591';
 
   $req->header('Cookie' => 'JSESSIONID=FD43E88157EE150E0C7270025AFCF18D');
@@ -14,8 +14,8 @@
   $req->content_type('application/x-www-form-urlencoded');
   #$req->content('accreditType=CARDOSD&duration=60&id=&message=ทดสอบระบบ&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=');
 
-  my $string  = 'accreditType=CARDOSD&duration=60&id=&message='.$name.'&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=';
-  $req->content( $string );
+  my $content  = 'accreditType=CARDOSD&duration=60&id=&message='.$message.'&operate=insert&remark=test&scCode='.$card.'&sendCount=1&sendInterval=60&sendTime='.$date.'%20'.$time.'&subAreaId=&subTypeId=&title=';
+  $req->content( $content );
 
   # Pass request to the user agent and get a response back
   my $res = $ua->request($req);
