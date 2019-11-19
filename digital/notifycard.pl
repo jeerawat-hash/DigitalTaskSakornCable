6a0caa3d417ca7ac14e31d67ffbc0698d3cbc707
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-19 11:27:26 +0700

    a

--- digital/notifycard.pl
+++ digital/notifycard.pl
@@ -13,7 +13,7 @@
   $req->content_type('application/x-www-form-urlencoded');
   #$req->content('accreditType=CARDOSD&duration=60&id=&message=ทดสอบระบบ&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=');
 
-  my string  = 'accreditType=CARDOSD&duration=60&id=&message=$name&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=';
+  my $string  = 'accreditType=CARDOSD&duration=60&id=&message=$name&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=';
   $req->content( $string );
 
   # Pass request to the user agent and get a response back
