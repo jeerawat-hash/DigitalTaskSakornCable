fb23a17662552deb4839680d082a5ec11b68e404
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-19 11:11:55 +0700

    add

--- digital/notifycard.pl
+++ digital/notifycard.pl
@@ -1,14 +1,17 @@
   # Create a user agent object
   use LWP::UserAgent;
+
   $ua = new LWP::UserAgent;
   $ua->agent("AgentName/0.1 " . $ua->agent);
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/camessageedit.do';
 
+  my ($name) = @ARGV;
+
   $req->header('Cookie' => 'JSESSIONID=FD43E88157EE150E0C7270025AFCF18D');
 
   $req->content_type('application/x-www-form-urlencoded');
-  $req->content('accreditType=CARDOSD&duration=60&id=&message=ทดสอบระบบ&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-17%2017:10:00&subAreaId=&subTypeId=&title=');
+  $req->content('accreditType=CARDOSD&duration=60&id=&message=ทดสอบระบบ&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=');
 
   # Pass request to the user agent and get a response back
   my $res = $ua->request($req);
@@ -20,4 +23,5 @@
   }
 
 
+  print $name;
 
