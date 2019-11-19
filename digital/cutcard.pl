e3eed2ec4c24c611a7ad8449bc0ed1c5b48ede02
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-19 12:46:41 +0700

    add

--- digital/cutcard.pl
+++ digital/cutcard.pl
@@ -5,6 +5,8 @@
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/avitcaaccredit.do';
 
+  my ($card,$message,$date,$time) = @ARGV;
+  
   $req->header('Cookie' => 'JSESSIONID=FD43E88157EE150E0C7270025AFCF18D');
 
   $req->content_type('application/x-www-form-urlencoded');
