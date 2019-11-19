2dd8099fd817439d68131c38869bf22a8d1b0c87
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-19 11:29:26 +0700

    add

--- digital/notifycard.pl
+++ digital/notifycard.pl
@@ -6,8 +6,9 @@
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/camessageedit.do';
 
-  my ($name) = @ARGV;
-
+  #my ($name) = @ARGV;
+  $name = '9980003200006591';
+  
   $req->header('Cookie' => 'JSESSIONID=FD43E88157EE150E0C7270025AFCF18D');
 
   $req->content_type('application/x-www-form-urlencoded');
