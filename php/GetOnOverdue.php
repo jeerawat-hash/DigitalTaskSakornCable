ee731c5d5d31dd2d94315588cadb99c41f315bf3
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-23 09:02:35 +0700

    add overdue and duedate

--- php/GetOnOverdue.php
+++ php/GetOnOverdue.php
@@ -21,12 +21,6 @@ where b.CustomerID = '".$resultdue["CustomerID"]."'
 GROUP BY  a.cardno,b.CustomerID ,c.CustomerName
  ORDER BY  startdate  desc "));
 
-
-    	print_r($cardno);
-
-
-/*
-
   		mssql_query(" 
   		INSERT INTO [LineBot].[dbo].[SMS_Digital_Ondue]
            ([CardNO]
@@ -36,16 +30,54 @@ GROUP BY  a.cardno,b.CustomerID ,c.CustomerName
            ,[WriteDate]
            ,[Status_Send])
      VALUES
-           ('".$resultdue[""]."'
+           ('".$cardno["cardno"]."'
            ,'".$resultdue["DB"]."'
            ,'".$resultdue["CustomerID"]."'
            ,'".$resultdue["PayCode"]."'
            ,'".date("Y-m-d")."'
            ,'0')
             ");  	
- */
+ 
+    }
 
 
+
+
+
+    $queryOverdue =  mssql_query(" select *  from LineBot.dbo.Overdue 
+where CustomerID not in ( select CustomerID from [LineBot].[dbo].[SMS_Digital_Overdue] ) 
+and CONVERT(date,InvoiceDate) not in ( select WriteDate from [LineBot].[dbo].[SMS_Digital_Overdue] )
+  ");
+
+    while ( $resultOverdue = mssql_fetch_array($queryOndue) ) {
+  	
+
+
+    	$cardno = mssql_fetch_array(mssql_query(" 
+select top 1   a.cardno,b.CustomerID ,c.CustomerName ,  MAX(b.startdate) as startdate 
+from ".$resultOverdue["DB"].".dbo.customercardlog  a  left outer join ".$resultOverdue["DB"].".dbo.CustomerCableType b on a.CardNO = b.CardID 
+LEFT OUTER JOIN ".$resultOverdue["DB"].".dbo.Customer c ON b.CustomerID = c.CustomerID 
+where b.CustomerID = '".$resultOverdue["CustomerID"]."'
+GROUP BY  a.cardno,b.CustomerID ,c.CustomerName
+ ORDER BY  startdate  desc "));
+
+  		mssql_query(" 
+  		INSERT INTO [LineBot].[dbo].[SMS_Digital_Overdue]
+           ([CardNO]
+           ,[DB]
+           ,[CustomerID]
+           ,[PayCode]
+           ,[WriteDate]
+           ,[Status_Send])
+     VALUES
+           ('".$cardno["cardno"]."'
+           ,'".$resultOverdue["DB"]."'
+           ,'".$resultOverdue["CustomerID"]."'
+           ,'".$resultOverdue["PayCode"]."'
+           ,'".date("Y-m-d")."'
+           ,'0')
+            ");  	
+ 
     }
 
 
