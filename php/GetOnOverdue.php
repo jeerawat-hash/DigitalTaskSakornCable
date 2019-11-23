8fc425c18f45ab8b5d9355ad8c8f7b93026f27e1
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-23 08:59:08 +0700

    test paycode

--- php/GetOnOverdue.php
+++ php/GetOnOverdue.php
@@ -10,16 +10,25 @@ and CONVERT(date,InvoiceDate) not in ( select WriteDate from [LineBot].[dbo].[SM
   ");
 
     while ( $resultdue = mssql_fetch_array($queryOndue) ) {
-    	
+  	
 
 
+    	$cardno = mssql_fetch_array(mssql_query(" 
+select top 1   a.cardno,b.CustomerID ,c.CustomerName ,  MAX(b.startdate) as startdate 
+from ".$resultdue["DB"].".dbo.customercardlog  a  left outer join ".$resultdue["DB"].".dbo.CustomerCableType b on a.CardNO = b.CardID 
+LEFT OUTER JOIN ".$resultdue["DB"].".dbo.Customer c ON b.CustomerID = c.CustomerID 
+where b.CustomerID = '".$resultdue["CustomerID"]."'
+GROUP BY  a.cardno,b.CustomerID ,c.CustomerName
+ ORDER BY  startdate  desc "));
 
-    	print_r($resultdue);
+
+    	print_r($cardno);
+
 
 /*
-	
-	
-INSERT INTO [LineBot].[dbo].[SMS_Digital_Ondue]
+
+  		mssql_query(" 
+  		INSERT INTO [LineBot].[dbo].[SMS_Digital_Ondue]
            ([CardNO]
            ,[DB]
            ,[CustomerID]
@@ -27,14 +36,14 @@ INSERT INTO [LineBot].[dbo].[SMS_Digital_Ondue]
            ,[WriteDate]
            ,[Status_Send])
      VALUES
-           ('".."'
-           ,'".."'
-           ,'".."'
-           ,'".."'
-           ,'".."'
+           ('".$resultdue[""]."'
+           ,'".$resultdue["DB"]."'
+           ,'".$resultdue["CustomerID"]."'
+           ,'".$resultdue["PayCode"]."'
+           ,'".date("Y-m-d")."'
            ,'0')
-
-*/
+            ");  	
+ */
 
 
     }
