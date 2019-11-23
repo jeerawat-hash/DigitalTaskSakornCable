cfcacaebbb146db2c9288c2f79e59fce294d54e0
jeerawat <s6202041520016@email.kmutnb.ac.th>
2019-11-23 09:38:50 +0700

    add over due sms

---
+++ php/SMSCardOndue.php
@@ -0,0 +1,62 @@
+<?php 
+
+
+	ini_set('mssql.charset', 'UTF-8');
+    $connection = mssql_connect('mssqlcon', 'sa', 'Sakorn123');
+
+   
+
+  	$query = mssql_query("
+
+  	SELECT TOP 5 [ID],DB
+      ,[CardNO]
+      ,[CustomerID]
+      ,[PayCode]
+      ,WriteDate
+      ,[Status_Send]
+  	FROM [LineBot].[dbo].[SMS_Digital_Ondue]
+  	where len([CardNO]) > 10 and WriteDate = CONVERT(date,GETDATE()) and Status_Send = 0
+
+ 	");
+
+  	while ( $result = mssql_fetch_array($query) ) {
+  		
+  		$message = "แจ้งเตือนบิลครบกำหนด กรุณาชำระค่าบริการก่อนถูกตัดสัญญาณ สามารถกดปุ่ม Menu ที่กล่องเพื่อปิดข้อความ";
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 08:50:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 10:50:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 12:00:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 15:00:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 17:00:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 20:00:00 ");
+
+  		sleep(5);
+
+  		shell_exec(" perl /var/www/html/schedue/digital/notifycard.pl ".$result["CardNO"]." ".$message." ".$result["WriteDate"]." 21:00:00 ");
+ 
+
+  		mssql_query(" update [LineBot].[dbo].[SMS_Digital_Ondue] set Status_Send = 1 where  [ID] = '".$result["ID"]."' ");
+
+  	}
+  
+
+		 
+
+
+
+ ?>
