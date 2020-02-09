47dba6620b7fd522186955835c9a2ca4ce3a3d0e
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-02-09 13:40:07 +0700

    add

--- php/api.php
+++ php/api.php
@@ -1,7 +1,10 @@
 <?php 
 
 		
-		///// Controller
+error_reporting(0);
+
+	/// 9980003000000039 
+	///// Controller
     $pull = file_get_contents("php://input");
     ///// Controller
 
@@ -9,6 +12,40 @@
 	$data = json_decode($pull,true);
 
 	print_r($data);
+
+	echo "<br>";
+
+	echo $data["CardNo"]."<br>".$data["Status"]."<br>";
+
+	/*
+	if ( isset($_POST["cutcard"]) ) {
+		
+		$string  = " perl /var/www/html/schedue/digital/cutcard.pl ".$_POST["card"]." ";
+
+		$exe =  shell_exec( $string );
+
+		if ($exe) {
+			echo "CUT ".$_POST["card"];
+		}
+
+	}else	
+	if ( isset($_POST["connectcard"]) ) {
+		
+		$string  = " perl /var/www/html/schedue/digital/opencard.pl ".$_POST["card"]." ";
+
+		$exe =  shell_exec( $string );
+
+		if ($exe) {
+			echo "Connect ".$_POST["card"];
+		}
+
+	}else{
+
+
+
+	}*/
+
+
 
 
  ?>
