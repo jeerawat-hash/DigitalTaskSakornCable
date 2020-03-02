0991c5a165f768009a54279fa19343386bd0f8bb
jeerawat-hash <54786014+jeerawat-hash@users.noreply.github.com>
2020-03-03 04:27:36 +0700

    Update cutcard.pl

--- digital/cutcard.pl
+++ digital/cutcard.pl
@@ -7,7 +7,7 @@
 
   my ($card,$message) = @ARGV;
 
-  $req->header('Cookie' => 'JSESSIONID=8D34C0DFB813070D5BE4837D513E5BA1');
+  $req->header('Cookie' => 'JSESSIONID=0E9554779CCADC628688F2D98E317915');
 
   $req->content_type('application/x-www-form-urlencoded');
   $req->content('accreditType=BUYPRO_1&duration=&message=&operate=accredit&position=0&productIds=81&scCode='.$card.'&title=');
