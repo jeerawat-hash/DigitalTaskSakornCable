6463cc95dc73e968364d8cd0c4187ac2b93dc4b9
jeerawat-hash <54786014+jeerawat-hash@users.noreply.github.com>
2020-04-14 02:50:03 +0700

    Update rescookie.pl

--- digital/rescookie.pl
+++ digital/rescookie.pl
@@ -5,7 +5,7 @@
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/searchcamessage.do';
 
-  $req->header('Cookie' => 'JSESSIONID=3C826DE6BA99F580BAA06D3A95D1B0E1');
+  $req->header('Cookie' => 'JSESSIONID=C4AE7C8BBE800D9761D690A9440D1DF0');
 
   $req->content_type('application/x-www-form-urlencoded');
   $req->content('accreditType=&endDate=&operate=info_search&page=&scCode=&startDate=&subAreaId=&subTypeId=');
