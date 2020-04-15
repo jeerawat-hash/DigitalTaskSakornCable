7761c087bda67eae4a9d0cbf52121055416faecd
jeerawat-hash <54786014+jeerawat-hash@users.noreply.github.com>
2020-04-16 06:36:58 +0700

    Update notifycard.pl

--- digital/notifycard.pl
+++ digital/notifycard.pl
@@ -9,7 +9,7 @@
   my ($card,$message,$date,$time) = @ARGV;
   #$name = '9980003200006591';
 
-  $req->header('Cookie' => 'JSESSIONID=18F3F230137FEA418D23EB4D7C41F5D6');
+  $req->header('Cookie' => 'JSESSIONID=AC2337BC66B1D20EE9E1758429F86D28');
 
   $req->content_type('application/x-www-form-urlencoded');
   #$req->content('accreditType=CARDOSD&duration=60&id=&message=ทดสอบระบบ&operate=insert&remark=test&scCode=9980003200006591&sendCount=1&sendInterval=60&sendTime=2019-11-19%2017:10:00&subAreaId=&subTypeId=&title=');
