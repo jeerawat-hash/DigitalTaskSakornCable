6f6da14a7a1a6444528f4af1b66453a283ad46bf
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-04-17 14:08:13 +0700

    add

--- php/Cardinit.php
+++ php/Cardinit.php
@@ -2,7 +2,7 @@
 
 
 	   ini_set('mssql.charset', 'UTF-8');
-    $connection = mssql_connect('mssqlconcas', 'check', 'sakorn123');
+/*    $connection = mssql_connect('mssqlconcas', 'check', 'sakorn123');
  
 
     $cas_str = mssql_query(" SELECT  [CardNO]
@@ -104,8 +104,140 @@
       }
 
  
-      echo $all;
+      echo $all;*/
 
+      ////////////////////////// begin cut out //////////////////////////////
+
+      $connection = mssql_connect('mssqlcon', 'sa', 'Sakorn123');
+
+      
+      $getAllCard_str = " SELECT top 10 [ID]
+            ,[CardNO]
+            ,[Is_Init]
+        FROM [WebSakorn].[dbo].[CardInit] where Is_Init = 0 order by ID ";
+
+
+      $Report = "";
+
+
+      while ($resultCard = mssql_fetch_array($getAllCard_str)) {
+        
+
+       
+
+            $Card_str =  mssql_query(" declare @sCardNo char(20)
+
+              set @sCardNo = '".$resultCard["CardNO"]."'
+
+
+              SELECT * FROM
+              (
+              select DB='SakornCable',a.customerid,b.CardID ,a.stopdate from
+              (select * from SakornCable.dbo.Customer where StopDate is null
+                                        and CustomerID in (select CustomerID from SakornCable.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join SakornCable.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+
+              UNION
+
+              select DB='SakornNetwork',a.customerid,b.CardID ,a.stopdate from
+              (select * from SakornNetwork.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from SakornNetwork.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join SakornNetwork.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+              UNION
+              select DB='Sakorp',a.customerid,b.CardID ,a.stopdate from
+              (select * from Sakorp.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from Sakorp.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join Sakorp.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+
+              UNION
+              select DB='CSCable',a.customerid,b.CardID ,a.stopdate from
+              (select * from CSCable.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from CSCable.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join CSCable.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+              UNION
+              select  DB='Sahamit',a.customerid,b.CardID ,a.stopdate from
+              (select * from Sahamit.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from Sahamit.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join Sahamit.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+              UNION
+
+               
+              select DB='Bangchalong',a.customerid,b.CardID ,a.stopdate from
+              (select * from Bangchalong.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from Bangchalong.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join Bangchalong.dbo.customercabletype b
+              on a.customerid = b.customerid  AND b.CardID = @sCardNo  
+
+              UNION
+
+              select DB='Flat',a.customerid,b.CardID ,a.stopdate from
+              (select * from Flat.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from Flat.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join Flat.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+
+              UNION
+              select DB='SRN',a.customerid,b.CardID ,a.stopdate from
+              (select * from SRN.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from SRN.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join SRN.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+
+              UNION
+              select DB='SakornNewBusiness',a.customerid,b.CardID ,a.stopdate from
+              (select * from SakornNewBusiness.dbo.Customer where StopDate is   null
+                                        and CustomerID in (select CustomerID from SakornNewBusiness.dbo.customercabletype where CardID = @sCardNo   )
+               ) a    Left outer join SakornNewBusiness.dbo.customercabletype b
+              on a.customerid = b.customerid   AND b.CardID = @sCardNo
+
+              ) SakornGroup ORDER BY StopDate desc
+
+               
+                "); 
+
+            
+            $CardStatus = mssql_num_rows($Card_str);
+            $StatusRemrk = "";
+
+            sleep(1);
+
+            if ($CardStatus == 0) {
+                ############ cut card number ##############
+
+                $string  = " perl /var/www/html/schedue/digital/cutcard.pl ".$resultCard["CardNO"]." ";
+
+                $exe =  shell_exec( $string );
+
+                ############ cut card number ##############
+                $StatusRemrk = "FreeCard";
+
+                $Report .= "ตัด ".$resultCard["CardNO"]."\n"
+
+            }else{
+
+
+
+                $StatusRemrk = "NormalCard";
+            }
+
+  
+
+            mssql_query("update [WebSakorn].[dbo].[CardInit] set [Is_Init] = 1 , Remark = '".$StatusRemrk."' where CardNo = '".$resultCard["CardNo"]."' ");
+
+  
+      }
+
+
+      $ReportAll = "SyncCardStatus by CasServer....\n".$Report;
+
+
+
+      notify($ReportAll,"Ahlxzwfwdnv7CjVPMC3s6fdNPtOEH49AeQkhF4CUfKI");
       
  
 
