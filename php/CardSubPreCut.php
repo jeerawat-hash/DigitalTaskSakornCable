25fadaa57b7c7b3ffb45c602a69c2bcc97089567
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2020-09-26 14:27:55 +0700

    add

---
+++ php/CardSubPreCut.php
@@ -0,0 +1,79 @@
+<?php 
+
+
+	   ini_set('mssql.charset', 'UTF-8'); 
+
+
+      ////////////////////////// begin cut out //////////////////////////////
+       
+      $connection = mssql_connect('mssqlcon', 'sa', 'Sakorn123');
+
+      
+      $query = mssql_query( " SELECT top 1  a.ID,b.Fname,[CardNo],[EmployeeID],Is_Open,Is_Success
+    FROM [WebSakorn].[dbo].[SubPreOpenCard] a 
+    join [WebSakorn].[dbo].[Employee] b on a.EmployeeID = b.ID  where Is_Success = 0 and Is_Open = 1 " );
+  
+
+      while ($result = mssql_fetch_array($query)) {
+         
+      sleep(3);
+      $string  = " perl /var/www/html/schedue/digital/cutcard.pl ".$result["CardNo"]." ";
+      $exe =  shell_exec( $string );
+
+
+      $message = "\nระบบตัดสัญญาณจากการเปิดชั่วคราวการ์ด\nหมายเลข : ".$result["CardNo"];
+
+      notify($message,"1JHQB0CgfO834Dnz0VNETIBtHgCm1d7qrjNP6HxJlCO");
+
+
+      mssql_query( " update [WebSakorn].[dbo].[SubPreOpenCard] set Is_Success = 1 where ID = '".$result["ID"]."' " );
+
+
+      }
+
+
+
+
+
+
+function notify($message,$token){
+
+          $lineapi = $token; 
+        $mms =  trim($message); 
+        date_default_timezone_set("Asia/Bangkok");
+        $con = curl_init();
+        curl_setopt( $con, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+        // SSL USE 
+        curl_setopt( $con, CURLOPT_SSL_VERIFYHOST, 0); 
+        curl_setopt( $con, CURLOPT_SSL_VERIFYPEER, 0); 
+        //POST 
+        curl_setopt( $con, CURLOPT_POST, 1); 
+        curl_setopt( $con, CURLOPT_POSTFIELDS, "message=$mms"); 
+        $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$lineapi.'', );
+          curl_setopt($con, CURLOPT_HTTPHEADER, $headers); 
+        curl_setopt( $con, CURLOPT_RETURNTRANSFER, 1); 
+        $result = curl_exec( $con ); 
+
+}
+
+
+function Operation($url,$clientid,$status)
+{
+    
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_POSTFIELDS,
+              "access_code=ZWaFcOV3yILx6DjmRWs029EzdYQgy0GdmoHA779tAK4vz8FqP55kOtivxounk11erUF7NsplanMKDivQVJL1pxIPwbkNEnBJSqCsfwXZcGZfXrSeTdezPt3CpUHYYR22fQjc6iGWwq8M&id=".$clientid."&status=".$status."&name=&surname=&mac_address&username&password&site_id=97&package_id=&type=");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  $result = curl_exec($ch);
+
+  curl_close ($ch);
+
+  return $result;
+}
+
+
+ ?>
