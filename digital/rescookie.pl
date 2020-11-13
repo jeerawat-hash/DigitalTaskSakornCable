51dd474584e06b6939534377991d05aaa4c2063a
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-11-13 20:28:44 +0700

    add

--- digital/rescookie.pl
+++ digital/rescookie.pl
@@ -4,9 +4,9 @@
   $ua->agent("AgentName/0.1 " . $ua->agent);
   # Create a request
   my $req = new HTTP::Request POST => 'http://172.168.2.2:8081/sms2_4/ca/searchcamessage.do';
-
+  
   $req->header('Cookie' => 'JSESSIONID=09E5E45BA1C79528AEBB851081FD497C');
-
+  
   $req->content_type('application/x-www-form-urlencoded');
   $req->content('accreditType=&endDate=&operate=info_search&page=&scCode=&startDate=&subAreaId=&subTypeId=');
 
