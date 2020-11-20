5128209bb5d40cdaaf68e1587f0da8b5849f956b
jeerawat <s6202041520016@email.kmutnb.ac.th>
2020-11-20 20:05:45 +0700

    add

---
+++ nex/cutcard.pl
@@ -0,0 +1,31 @@
+  # Create a user agent object
+  use LWP::UserAgent;
+  
+  $ua = new LWP::UserAgent;
+  $ua->agent("AgentName/0.1 " . $ua->agent);
+
+  my ($AccountID) = @ARGV;
+
+  # Create a request
+  my $req = new HTTP::Request POST => 'http://agent.cdnisp.com/bill/customer/edit/id/'.$AccountID.'.html';
+  
+  $req->header('Cookie' => 'PHPSESSID=f6e3jp56h51lnopkmqbn88oke6');
+  
+  $req->content_type('application/x-www-form-urlencoded');
+  $req->content('is_enable=0');
+
+  # Pass request to the user agent and get a response back
+  my $res = $ua->request($req);
+  # Check the outcome of the response
+  if ($res->is_success) {
+ 
+
+      print $res->content;
+
+
+  } else {
+      print "Bad luck this time\n";
+  }
+
+
+# perl /var/www/html/schedue/nex/cutcard.pl 9980003000000773
