d4b71a6d4811df38c90b4d36eabf2119cf2df8c9
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2021-09-01 22:34:14 +0700

    add

--- php/CardNoEntitlement.php
+++ php/CardNoEntitlement.php
@@ -116,7 +116,14 @@
           
           $MSGT = "[ระบบ]\nย้ำสัญญาณการ์ดหมายเลข ".$Result["CardNO"]." สำเร็จแล้ว\nกรุณาทดสอบเปลี่ยนช่องใน 1-2 นาทีหากภาพไม่มาภายใน 30 นาทีกรุณาแจ้งงานบริการอีกครั้ง";
 
-          botpush($token,$channel,"U9c4cfc5da6f9aedeafd692b67b37a59e",$MSGT);
+          if(isset($Result["LineID"])){
+            
+            #botpush($token,$channel,"U9c4cfc5da6f9aedeafd692b67b37a59e",$MSGT);
+            botpush($token,$channel,$Result["LineID"],$MSGT);
+
+          }
+
+          
 
       }
 
