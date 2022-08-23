9c901aa85e0bd4e848ff5f31a680b8b3b92035b8
jeerawat-hash <s6202041520016@email.kmutnb.ac.th>
2022-08-23 20:01:32 +0700

    add

--- php/PPPOEScan.php
+++ php/PPPOEScan.php
@@ -10,17 +10,24 @@
       $connection = mssql_connect('mssqlcon', 'sa', 'Sakorn123');
 
       
-      $query = mssql_query( "
+      /*$query = mssql_query( "
       select SeqNo,CustomerID,PPOE,INetID,SakornID,SakornIPID,
 (case when substring(PPOE,1,12)='sakorn_cable' then 'S'
 	  when substring(PPOE,1,13)='sakorn_public' then 'SP' else 'I' end)  as TypeNET
 from sakorncable.dbo.CustomerCableType where PPOE is not null and (SakornID is null and INetID is null and SakornIPID is null) and len(PPOE) > 12
 order by TypeNET asc 
-     " );
+     " );*/
 
 
 
-      while ($result = mssql_fetch_array($query)) {
+     $url = "https://bb.inet-th.net/index.php/api/clients";
+
+     $clientsArray = json_decode(getclient($url),true);
+
+     print_r($clientsArray);
+
+
+     //while ($result = mssql_fetch_array($query)) {
 
         /*if($result["TypeNET"] == "S"){
 
@@ -28,27 +35,27 @@ order by TypeNET asc
 
             mssql_query("update sakorncable.dbo.CustomerCableType set SakornID = '".$resultA["ID"]."'  where SeqNo = '".$result["SeqNo"]."' and CustomerID = '".$result["CustomerID"]."' ");
 
-        }else*/
+        }else
         if($result["TypeNET"] == "SP"){
 
             $resultA = mysql_fetch_array(mysql_query(" SELECT ID FROM radcheck where username = '".$result["PPOE"]."' "));
 
             mssql_query("update sakorncable.dbo.CustomerCableType set SakornIPID = '".$resultA["ID"]."' where SeqNo = '".$result["SeqNo"]."' and CustomerID = '".$result["CustomerID"]."' ");
 
-        }
+        }*/
+    
         /*
         if($result["TypeNET"] == "I"){
 
+            $url = "https://bb.inet-th.net/index.php/api/clients";
 
-
-
-
+            $clientsArray = json_decode(getclient($url),true);
 
             mssql_query(" update sakorncable.dbo.CustomerCableType set INetID = '' where SeqNo = '".$result["SeqNo"]."' and CustomerID = '".$result["CustomerID"]."' ");
             
-        }*/
-
-      }
+        }
+            */
+      //}
 
  
 
@@ -74,23 +81,26 @@ function notify($message,$token){
 
 }
 
-function Operation($url,$clientid,$status)
+
+function getclient($url)
 {
     
-  $ch = curl_init();
+	$ch = curl_init();
 
-  curl_setopt($ch, CURLOPT_URL,$url);
+	curl_setopt($ch, CURLOPT_URL,$url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS,
-              "access_code=ZWaFcOV3yILx6DjmRWs029EzdYQgy0GdmoHA779tAK4vz8FqP55kOtivxounk11erUF7NsplanMKDivQVJL1pxIPwbkNEnBJSqCsfwXZcGZfXrSeTdezPt3CpUHYYR22fQjc6iGWwq8M&id=".$clientid."&status=".$status."&name=&surname=&mac_address&username&password&site_id=97&package_id=&type=");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS,
+	            "access_code=ZWaFcOV3yILx6DjmRWs029EzdYQgy0GdmoHA779tAK4vz8FqP55kOtivxounk11erUF7NsplanMKDivQVJL1pxIPwbkNEnBJSqCsfwXZcGZfXrSeTdezPt3CpUHYYR22fQjc6iGWwq8M&type&search&page=1&client_per_page=999999&inc_terminate=no");
+	// In real life you should use something like:
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+	//          http_build_query(array('postvar1' => 'value1')));
 
-  $result = curl_exec($ch);
+	// Receive server response ...
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-  curl_close ($ch);
+	$result = curl_exec($ch);
 
-  return $result;
+	curl_close ($ch);
+
+	return $result;
 }
-
-
- ?>
